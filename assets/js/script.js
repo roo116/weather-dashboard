4a685303ed2a92a6079a0604ad01d560dd5a412b
roo <roo116@nc.rr.com>
2022-05-24 21:42:22 -0400

    basic file structure and index.html

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var apiUrl = 'https://openweathermap.org/api/one-call-api'
+//get current weather
+//get 7-day forcast - 2-days
+
+
+function getWeatherForcast ()
+
+// var getUserRepos = function(user) {
+//     // format the github api url
+//     // var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+  
+//     // make a get request to url
+//     fetch(apiUrl)
+//       .then(function(response) {
+//         // request was successful
+//         if (response.ok) {
+//           console.log(response);
+//           response.json().then(function(data) {
+//             console.log(data);
+//             displayRepos(data, user);
+//           });
+//         } else {
+//           alert('Error: GitHub User Not Found');
+//         }
+//       })
+//       .catch(function(error) {
+//         alert('Unable to connect to GitHub');
+//       });
+//   };
+  
