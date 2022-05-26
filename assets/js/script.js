00079ffa5b962f7f54b772818fb5164958430db0
roo116 <roo116@nc.rr.com>
2022-05-26 16:58:48 -0400

    weather icon code working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,79 +10,82 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-
 // var apiUrlFcast = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=10bdd765350b62b1d956051bc4e6292c";
 // var apiUrlCurr = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 // var apiUrlUvi = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
 
-
 var inputEl = document.querySelector("input");
 var btnEl = document.querySelector(".btn");
-var secret = "10bdd765350b62b1d956051bc4e6292c"
-var cityArr = []
+var secret = "10bdd765350b62b1d956051bc4e6292c";
+var cityArr = [];
 
 btnEl.addEventListener("click", function (event) {
-    event.preventDefault();
-    inputEl = inputEl.value;
-    console.log(inputEl);
-    inputEl.trim();
-    inputEl = inputEl.toLowerCase();
-    // inputElArr = inputEl.split(" ");
+  event.preventDefault();
+  inputEl = inputEl.value;
+  console.log(inputEl);
+  inputEl.trim();
+  inputEl = inputEl.toLowerCase();
+  // inputElArr = inputEl.split(" ");
 
-    // if (inputElArr.length > 1) {
-    //     for (i = 0; i < inputElArr.length; i++) {
-    //         cityArr.push(inputElArr[i], "&")
-    //     }
-    //     var cityName = cityArr.join("");
-    // } else {
-    var cityName = inputEl;
-    // }
-    console.log(cityName);
-    document.getElementById("city-input").value = ""
+  // if (inputElArr.length > 1) {
+  //     for (i = 0; i < inputElArr.length; i++) {
+  //         cityArr.push(inputElArr[i], "&")
+  //     }
+  //     var cityName = cityArr.join("");
+  // } else {
+  var cityName = inputEl;
+  // }
+  console.log(cityName);
+  document.getElementById("city-input").value = "";
 
+  //build the query
+  // apiUrl2 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + secret + "";
 
-    //build the query
-    // apiUrl2 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + secret + "";
+  // 1. fetch data
+  var testApi1 =
+    "https://api.openweathermap.org/data/2.5/weather?q=chapel%20hill&appid=10bdd765350b62b1d956051bc4e6292c";
+    var wData = ""
 
-    // 1. fetch data
-    var testApi1 = "https://api.openweathermap.org/data/2.5/weather?q=chapel%20hill&appid=10bdd765350b62b1d956051bc4e6292c";
+  fetch(testApi1).then(function (response) {
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function (data) {
+          wData = data;
+        console.log("this is ", wData);    
+      });
+    }
+  });
 
-    fetch(testApi1).then(function (response) {
-        if (response.ok) {
-
-        }
-    })
-
-
-
-    // console.log(apiUrl2)
-
-    // window.open(apiUrl2);
-
-
-
-    // START cuttent conditions card
-    // return cityName
-    // date -- jscript Date()
-
-    // from apiUrl2
-    // weather icon -- .weather.icon()
-    // temp -- .main.temp()
-    // wind -- .wind.speed()
-    // humidity -- .main.humidity()
-    // UV index
-    // chapel hill test -- current.uvi()
-    // https://api.openweathermap.org/data/2.5/onecall?lat=35.9132&lon=-79.0558&exclude=hourly,daily&appid=10bdd765350b62b1d956051bc4e6292c
-    //
+//   construct the icon and get that out of the way
+var iconCode = wData.weather[0].icon;
+var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+var iconEl = document.createElement("img");
+iconEl.setAttribute("src", iconUrl);
+var wIiconEl = document.querySelector("#wIcon");
+wIiconEl.appendChild(iconEl);
+iconEl.setAttribute("src", iconUrl);
 
 
 
 
+  // console.log(apiUrl2)
 
+  // window.open(apiUrl2);
 
+  // START cuttent conditions card
+  // return cityName
+  // date -- jscript Date()
+
+  // from apiUrl2
+  // weather icon -- .weather.icon()
+  // temp -- .main.temp()
+  // wind -- .wind.speed()
+  // humidity -- .main.humidity()
+  // UV index
+  // chapel hill test -- current.uvi()
+  // https://api.openweathermap.org/data/2.5/onecall?lat=35.9132&lon=-79.0558&exclude=hourly,daily&appid=10bdd765350b62b1d956051bc4e6292c
+  //
 });
-
-
 
 // NOT STARTED
 // get current weather
